647c62feafc78441cc47de52fc868cc95f02b57a
BiancoSofia24 <b.sofia2124@gmail.com>
2021-10-11 16:06:15 -0300

    players score

--- exercises/player-score-counter/src/App.js
+++ exercises/player-score-counter/src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [p1score, setP1score] = useState(0);
+  const [p2score, setP2score] = useState(0);
+
+  const increaseP1Score = () => {
+    setP1score(p1score + 1);
+  }
+  
+  const decreaseP1Score = () => {
+    setP1score(p1score - 1);
+  }
+  
+  const increaseP2Score = () => {
+    setP2score(p2score + 1);
+  }
+  
+  const decreaseP2Score = () => {
+    setP2score(p2score - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Player Counter!</h1>
+
+      <section>
+        <div className="player">
+          <h3>Player 1</h3>
+          <span>Score: {p1score}</span>
+          <div className="btns">
+            <button onClick={increaseP1Score}>Increase Score</button>
+            <button onClick={decreaseP1Score}>Decrease Score</button>
+          </div>
+        </div>
+
+        <div className="player">
+          <h3>Player 2</h3>
+          <span>Score: {p2score}</span>
+          <div className="btns">
+            <button onClick={increaseP2Score}>Increase Score</button>
+            <button onClick={decreaseP2Score}>Decrease Score</button>
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 }
